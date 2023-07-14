e01012cc885260496803b1c3b7d8503325a62144
Thimo Limpert (q.beyond AG) <thimo.limpert@qbeyond.de>
2023-07-14 16:26:44 +0200

    forgot to deploy the VNet

--- policy_definitions/vhub_connections/tests/2_same_resource_group/main.tf
+++ policy_definitions/vhub_connections/tests/2_same_resource_group/main.tf
@@ -12,3 +12,12 @@ module "setup_policy" {
   virtual_hub_id = module.setup_hub.virtual_hub.id
   scope          = module.setup_hub.resource_group.id
 }
+
+resource "azurerm_virtual_network" "exercise" {
+  count               = var.exercise ? 1 : 0
+  name                = "vnet-192-168-1-0-24-westeurope"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = ["192.168.1.0/24"]
+}
+

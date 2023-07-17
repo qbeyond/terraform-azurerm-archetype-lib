1c686537400d59859553429f22526ffaeade15e6
Thimo Limpert (q.beyond AG) <thimo.limpert@qbeyond.de>
2023-07-17 08:49:38 +0200

    fix undeclared resource reference

--- policy_definitions/vhub_connections/tests/2_same_resource_group/main.tf
+++ policy_definitions/vhub_connections/tests/2_same_resource_group/main.tf
@@ -16,8 +16,8 @@ module "setup_policy" {
 resource "azurerm_virtual_network" "exercise" {
   count               = var.exercise ? 1 : 0
   name                = "vnet-192-168-1-0-24-westeurope"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = module.setup_hub.resource_group.location
+  resource_group_name = module.setup_hub.resource_group.name
   address_space       = ["192.168.1.0/24"]
 }
 

525b92dd405df27737a6d51b10c007d9615173b5
Thimo Limpert (q.beyond AG) <thimo.limpert@qbeyond.de>
2024-02-21 15:41:51 +0100

    add output to allow manual evaluation if needed

--- tests/policy_definition/update_management/main.tf
+++ tests/policy_definition/update_management/main.tf
@@ -131,3 +131,8 @@ resource "azapi_resource_action" "evaluation" {
   resource_id = "${azurerm_resource_group.this.id}/providers/Microsoft.PolicyInsights/policyStates/latest"
   depends_on  = [azurerm_resource_group_policy_assignment.deploy_maintenance_resources]
 }
+
+output "evaluation_trigger_command" {
+  description = "Command to trigger a policy evaluation on the resource group manually. This is automatically done on changes of the policy assignment."
+  value       = "az policy state trigger-scan --resource-group ${azurerm_resource_group.this.name}"
+}

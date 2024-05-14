7f4bbc5bcf74a185e4400744cd857400e192c1e7
Inga Müller <inga.mueller@qbeyond.de>
2024-05-14 15:43:26 +0200

    fix: subnetcreation and nsg assosiation is now inline with vnet-creation

--- policy_definitions/network_security/tests/snet_01_namingconform_correct_nsg_attached/main.tf
+++ policy_definitions/network_security/tests/snet_01_namingconform_correct_nsg_attached/main.tf
@@ -27,16 +27,10 @@ resource "azurerm_virtual_network" "this" {
     address_space       = ["10.0.0.0/16"]
     location            = azurerm_resource_group.this.location
     resource_group_name = azurerm_resource_group.this.name
-}
 
-resource "azurerm_subnet" "snet_10_0_1_0_24" {
-    name                 = "snet-10-0-1-0-24-TestingPolicies"
-    resource_group_name  = azurerm_resource_group.this.name
-    virtual_network_name = azurerm_virtual_network.this.name
-    address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_subnet_network_security_group_association" "this" {
-    subnet_id                 = azurerm_subnet.snet_10_0_1_0_24.id
-    network_security_group_id = azurerm_network_security_group.this.id
+  subnet {
+    name           = "snet-10-0-1-0-24-TestingPolicies"
+    address_prefix = "10.0.1.0/24"
+    security_group = azurerm_network_security_group.this.id
+  }
 }
